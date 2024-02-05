340303b1abfddf26dcd5ae299a9536489853a647
amir z <aafz1382@gmail.com>
2024-02-05 23:37:24 +0330

    type-fixed

--- src/features/authentication/SignUpForm.jsx
+++ src/features/authentication/SignUpForm.jsx
@@ -3,7 +3,7 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-import { useSignup } from "./useSignup";
+import { useSignup } from "./useSignUp";
 
 // Email regex: /\S+@\S+\.\S+/
 
